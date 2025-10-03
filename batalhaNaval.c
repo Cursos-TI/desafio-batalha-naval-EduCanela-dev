fcfa7970e6babcc490310a416a0cff9861e9fe2f
Eduardo.m-dev <eduardomoreira.o2005@gmail.com>
2025-10-03 01:00:45 +0000

    batalha naval - nível novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,11 +4,72 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    #include <stdio.h>
+
+int main(){
+
+   
+    int tabuleiro[10][10];
+    
+    char NUM_colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int NUM_linhas = 0;
+    int linha = 0;
+    
+    
+    
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            tabuleiro[i][j] = 0;
+        }
+        
+    }
+    
+    
+    printf("\n\n");
+    
+    
+    tabuleiro[8][8] = 3;
+    tabuleiro[8][7] = 3;
+    tabuleiro[8][6] = 3;
+
+    tabuleiro[1][3] = 3;
+    tabuleiro[2][3] = 3;
+    tabuleiro[3][3] = 3;
+    
+
+
+    printf("      ");//espaço que define a letra da coluna
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%c ", NUM_colunas[i]);
+    }
+
+
+            printf("\n\n");
+
+
+    
+    while (linha < 10)
+    {
+        printf("%2d|   ", NUM_linhas++);//define o numero da linha
+            for (int coluna = 0; coluna < 10; coluna++)
+            {
+                printf("%d ", tabuleiro[linha][coluna]);
+            }
+        linha++;
+        printf("\n");        
+    }
+    
+
+return 0;}
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -36,5 +97,4 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
-}
+ 
