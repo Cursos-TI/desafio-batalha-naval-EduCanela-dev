cdd8ffe99063d2d2be1e11cab4013e36ac998303
Eduardo.m-dev <eduardomoreira.o2005@gmail.com>
2025-10-03 23:54:52 +0000

    batalha naval - nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,101 +1,68 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    #include <stdio.h>
+#define LINHAS  10
+#define COLUNAS 10
+#define LOCALIZACAO 0
 
 int main(){
 
-   
-    int tabuleiro[10][10] = {0};//matriz tabuleiro
-    
-    char NUM_colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};//numero das colunas
-    int NUM_linhas = 0; //nomeando as linhas
-    int linha = 0; //declarando o numero de quantas linhas vão ter no tabuleiro
-    
-    
-    //definindo iniciais/locais do tabuleiro
-    for (int i = 0; i < 10; i++)
+    int tabuleiro[LINHAS][COLUNAS];
+    char NUM_colunas = 'A';//DEFINE A LETRA DA COLUNA NO TABULEIRO
+    int NUM_LINHAS = 0;//DEFINE O NUMERO DA LINHA NO TABULEIRO
+
+    //INICIALIZAÇÃO DO TABULEIRO
+    for (int i = 0; i < LINHAS; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < COLUNAS; j++)
         {
-            tabuleiro[i][j] = 0;
+            tabuleiro[i][j] = LOCALIZACAO;//"LOCALIZACAO" FOI PUXADO PARA DEFINIR VALORES QUE "i" E "j" VÃO PRINTAR
         }
     }
     
-    
-            printf("\n\n");
-    
-    //declarando inicial de cada navio
-    //navio 1
-    tabuleiro[8][8] = 3;
-    tabuleiro[8][7] = 3;
-    tabuleiro[8][6] = 3;
 
-    //navio 2
-    tabuleiro[1][3] = 3;
-    tabuleiro[2][3] = 3;
-    tabuleiro[3][3] = 3;
-    
-
-    //nomeando as colunas
-    printf("      ");//espaço que define a letra da coluna
-    for (int i = 0; i <= 10; i++)
+    //NAVIO 1 : HORIZONTAL
+    for (int j = 6; j <= 8; j++)
     {
-        printf("%c ", NUM_colunas[i]);
-    }
-
-
-            printf("\n\n");
-
-
-    
-    while (linha < 10)//até quando vai ser executado
-    {
-        printf("%2d|   ", NUM_linhas++);//define o numero da linha
-            for (int coluna = 0; coluna < 10; coluna++)
-            {
-                printf("%d ", tabuleiro[linha][coluna]);//exibição do tabuleiro
-            }
-        linha++;//impedir um loop infinito
-        printf("\n");//pular pra próxima linha  
+        tabuleiro[8][j] = 3;
     }
     
+    //NAVIO 2 : VERTICAL
+    for (int i = 1; i <= 3; i++)
+    {
+        tabuleiro[i][4] = 3;
+    }
+    
+    //NAVIO 3 : DIAGONAL
+    for (int j=9, i=0 ; j>=7, i<=2 ; j--, i++)
+    {
+        tabuleiro[i][j] = 3;   
+    }
+    
+    //NAVIO 4 : DIAGONAL
+    for (int i=8, j=1 ; i>=6, j<=3 ; i--, j++)
+    {        
+        tabuleiro[i][j] = 3;
+    }
+    
+    
+    printf("      ");//ESPAÇO PARA EXIBIR A LETRA DA COLUNA
+    for (int i = 0; i < COLUNAS; i++)//"COLUNAS" FOI PUXADO PARA DEFINIR QUANTAS LETRAS VÃO SER GERADAS
+    {
+        printf("%c ", NUM_colunas);
+        NUM_colunas++;//SOMANDO MAIS UMA LETRA
+    }
+    
+        printf("\n");
+    
+    for (int i = 0; i < LINHAS; i++)//"LINHAS" FOI PUXADO PARA DEFINIR QUANTAS LINHAS VÃO TER
+    {
+        printf("%d |   ", NUM_LINHAS++);//DEFINE O NUMERO DA COLUNA E ALINHA O TABULEIRO COM AS LETRAS DE CADA COLUNA
+        for (int j = 0; j < COLUNAS; j++)//"COLUNAS" FOI PUXADO PARA DEFINIR QUANTAS LETRAS VÃO SER GERADAS
+        {
+            printf("%d ", tabuleiro[i][j]);//EXIBIBIÇÃO DO TABULEIRO
+        }
+        printf("\n");
+    }
+        
 
 return 0;}
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
- 
