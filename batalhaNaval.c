3e1ce06ebcb326858b76b579cd1b8fc0eb742771
Eduardo.m-dev <eduardomoreira.o2005@gmail.com>
2025-10-05 00:09:18 +0000

    batalha naval - nivel mestre.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,65 +4,87 @@
 #define COLUNAS 10
 #define LOCALIZACAO 0
 
-int main(){
-
+int main() {
     int tabuleiro[LINHAS][COLUNAS];
-    char NUM_colunas = 'A';//DEFINE A LETRA DA COLUNA NO TABULEIRO
-    int NUM_LINHAS = 0;//DEFINE O NUMERO DA LINHA NO TABULEIRO
+    char NUM_colunas = 'A';
+    int NUM_LINHAS = 0;
 
-    //INICIALIZAÇÃO DO TABULEIRO
-    for (int i = 0; i < LINHAS; i++)
-    {
-        for (int j = 0; j < COLUNAS; j++)
-        {
-            tabuleiro[i][j] = LOCALIZACAO;//"LOCALIZACAO" FOI PUXADO PARA DEFINIR VALORES QUE "i" E "j" VÃO PRINTAR
+    // Inicializa o tabuleiro com 0
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            tabuleiro[i][j] = LOCALIZACAO;
         }
     }
-    
 
-    //NAVIO 1 : HORIZONTAL
-    for (int j = 6; j <= 8; j++)
-    {
+    // NAVIO 1: HORIZONTAL
+    for (int j = 6; j <= 8; j++) {
         tabuleiro[8][j] = 3;
     }
-    
-    //NAVIO 2 : VERTICAL
-    for (int i = 1; i <= 3; i++)
-    {
+
+    // NAVIO 2: VERTICAL
+    for (int i = 1; i <= 3; i++) {
         tabuleiro[i][4] = 3;
     }
-    
-    //NAVIO 3 : DIAGONAL
-    for (int j=9, i=0 ; j>=7, i<=2 ; j--, i++)
-    {
-        tabuleiro[i][j] = 3;   
-    }
-    
-    //NAVIO 4 : DIAGONAL
-    for (int i=8, j=1 ; i>=6, j<=3 ; i--, j++)
-    {        
+
+    // NAVIO 3: DIAGONAL PRINCIPAL
+    for (int j = 9, i = 0; j >= 7 && i <= 2; j--, i++) {
         tabuleiro[i][j] = 3;
     }
-    
-    
-    printf("      ");//ESPAÇO PARA EXIBIR A LETRA DA COLUNA
-    for (int i = 0; i < COLUNAS; i++)//"COLUNAS" FOI PUXADO PARA DEFINIR QUANTAS LETRAS VÃO SER GERADAS
-    {
-        printf("%c ", NUM_colunas);
-        NUM_colunas++;//SOMANDO MAIS UMA LETRA
+
+    // NAVIO 4: DIAGONAL SECUNDÁRIA
+    for (int i = 8, j = 1; i >= 6 && j <= 3; i--, j++) {
+        tabuleiro[i][j] = 3;
+    }
+
+    // HABILIDADE: CRUZ
+    for (int j = 9; j >= 7; j--) {
+        for (int i = 4; i <= 6; i++) {
+            tabuleiro[i][8] = 1;
+        }
+        tabuleiro[5][j] = 1;
+    }
+
+    // HABILIDADE: OCTAEDRO
+    for (int i = 7; i <= 9; i++) {
+        tabuleiro[i][3] = 1;
+        for (int j = 2; j <= 4; j++) {
+            tabuleiro[8][j] = 1;
+        }
+    }
+
+    // HABILIDADE: CONE
+    int verticeLinha = 2;
+    int verticeColuna = 2;
+    int alturaCone = 3;
+
+    for (int i = 0; i < alturaCone; i++) {
+        int linha = verticeLinha + i;
+        int inicioColuna = verticeColuna - i;
+        int fimColuna = verticeColuna + i;
+
+        for (int j = inicioColuna; j <= fimColuna; j++) {
+            if (linha >= 0 && linha < LINHAS && j >= 0 && j < COLUNAS) {
+                tabuleiro[linha][j] = 1;
+            }
+        }
     }
     
-        printf("\n");
     
-    for (int i = 0; i < LINHAS; i++)//"LINHAS" FOI PUXADO PARA DEFINIR QUANTAS LINHAS VÃO TER
-    {
-        printf("%d |   ", NUM_LINHAS++);//DEFINE O NUMERO DA COLUNA E ALINHA O TABULEIRO COM AS LETRAS DE CADA COLUNA
-        for (int j = 0; j < COLUNAS; j++)//"COLUNAS" FOI PUXADO PARA DEFINIR QUANTAS LETRAS VÃO SER GERADAS
-        {
-            printf("%d ", tabuleiro[i][j]);//EXIBIBIÇÃO DO TABULEIRO
+
+    // IMPRESSÃO DO TABULEIRO
+    printf("    ");
+    for (int i = 0; i < COLUNAS; i++) {
+        printf(" %c", NUM_colunas++);
+    }
+    printf("\n");
+
+    for (int i = 0; i < LINHAS; i++) {
+        printf("%2d |", NUM_LINHAS++);
+        for (int j = 0; j < COLUNAS; j++) {
+            printf(" %d", tabuleiro[i][j]);
         }
         printf("\n");
     }
-        
 
-return 0;}
+    return 0;
+}
